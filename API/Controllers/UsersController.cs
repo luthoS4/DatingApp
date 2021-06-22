91187bda8179a43360e0330ca00f1142449a1ff1
luthoS4 <lutho.mpofu@s4.co.za>
2021-06-22 13:42:32 +0200

    End of Section 3

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -16,7 +16,6 @@ namespace API.Controllers
         public UsersController(DataContext context)
         {
             _context = context;
-
         }
 
         [HttpGet]
--- API/Startup.cs
+++ API/Startup.cs
@@ -60,6 +60,9 @@ namespace API
 
             app.UseRouting();
 
+            //add policy to allow api
+            app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200") );
+
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
