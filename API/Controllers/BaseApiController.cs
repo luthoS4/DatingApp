3bafe134264d284f93d2e3df30f8cdaca8943344
luthoS4 <lutho.mpofu@s4.co.za>
2021-07-05 15:27:57 +0200

    End of section 4

---
+++ API/Controllers/BaseApiController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [ApiController] //attribute
+    [Route("api/[controller]")]
+    public class BaseApiController : ControllerBase
+    {
+        
+    }
+}
